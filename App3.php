2a3b3fb49ba2bce1642821f864b5def088c56ad6
gastonlopez27 <chrono27>
2016-08-29 19:46:06 -0300

    App3

---
+++ App3.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+// App3
+
+$a=3;
+$b=1;
+$c=7;
+
+$digitos = array();
+
+$digitos[] = $a;
+$digitos[] = $b;
+$digitos[] = $c;
+
+
+
+sort($digitos);
+
+echo "El numero del medio es: $digitos[1]"
+?>
+</body>
+</html>
