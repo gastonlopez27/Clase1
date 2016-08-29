48ddc97f1a43a4e8fe6e67055812a7a5f05d33dd
gastonlopez27 <chrono27>
2016-08-29 20:38:16 -0300

    App4 creada

--- App4.php
+++ App4.php
@@ -7,6 +7,20 @@
 
 // App 4
 
+$x=1;
+$y=0;
+$operator = "+";
+
+switch ($operator) {
+	case '+':
+		echo "$x + $y = ".($x + $y);
+		break;
+	
+	default:
+		# code...
+		break;
+}
+
 ?>
 </body>
 </html>
